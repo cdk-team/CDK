b17335691b468305a6ede53828705f3474d2dc80
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2021-05-14 10:33:33 +0300

    Add check for CAP_SYS_MODULE and CAP_DAC_READ_SEARCH

--- pkg/evaluate/available_linux_capabilities.go
+++ pkg/evaluate/available_linux_capabilities.go
@@ -27,6 +27,20 @@ func GetProcCapabilities() bool {
 		if len(lst) == 2 {
 			capStr := strings.TrimSpace(lst[1])
 			fmt.Printf("\tCap decode: 0x%s = %s\n", capStr, capability.CapHexToText(capStr))
+
+			caps, err := capability.CapHexParser(capStr)
+			if err != nil {
+				log.Printf("[-] capability.CapHexParser: %v\n", err)
+				return false
+			}
+			for _, c := range caps {
+				switch c {
+				case "CAP_DAC_READ_SEARCH":
+					fmt.Printf("[!] CAP_DAC_READ_SEARCH enabled. You can read files from host. Use 'cdk run cap-dac-read-search' ... for exploitation.")
+				case "CAP_SYS_MODULE":
+					fmt.Printf("[!] CAP_SYS_MODULE enabled. You can escape the container via loading kernel module. More info at https://xcellerator.github.io/posts/docker_escape/.")
+				}
+			}
 		}
 
 		if strings.Contains(matched, "3fffffffff") {
