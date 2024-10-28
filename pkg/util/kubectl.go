fdd8dd7df2bd1d6cd9d90a17a11aef1c42a661e5
neargle <yourneargo@gmail.com>
2024-10-28 22:24:34 +0800

    add util: new KubeConfig struct

--- pkg/util/kubectl.go
+++ pkg/util/kubectl.go
@@ -1,5 +1,45 @@
 package util
 
+type KubeConfig struct {
+	APIVersion     string    `yaml:"apiVersion"`
+	Clusters       []Cluster `yaml:"clusters"`
+	Contexts       []Context `yaml:"contexts"`
+	CurrentContext string    `yaml:"current-context"`
+	Kind           string    `yaml:"kind"`
+	Preferences    struct{}  `yaml:"preferences"`
+	Users          []User    `yaml:"users"`
+}
+
+type Cluster struct {
+	Cluster ClusterInfo `yaml:"cluster"`
+	Name    string      `yaml:"name"`
+}
+
+type ClusterInfo struct {
+	CertificateAuthorityData string `yaml:"certificate-authority-data"`
+	Server                   string `yaml:"server"`
+}
+
+type Context struct {
+	Context ContextInfo `yaml:"context"`
+	Name    string      `yaml:"name"`
+}
+
+type ContextInfo struct {
+	Cluster string `yaml:"cluster"`
+	User    string `yaml:"user"`
+}
+
+type User struct {
+	Name string   `yaml:"name"`
+	User UserInfo `yaml:"user"`
+}
+
+type UserInfo struct {
+	ClientCertificateData string `yaml:"client-certificate-data"`
+	ClientKeyData         string `yaml:"client-key-data"`
+}
+
 func RunKubectlCmd(args ...string) (string, error) {
 	var stdoutStr string
 	var err error
